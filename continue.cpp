5b988f5cbc1e4f1ca7c27ba22b8f0691ff6e9dd6
ksp-Abhishek <abhishekkashyapa13@gmail.com>
2024-07-29 23:04:06 +0530

    continue-statement

---
+++ continue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{ 
+    int n;
+
+    for(int i=0; i<=5; i++){
+        cout<<"hi";
+        cout<<"hay";
+        continue;
+        cout<<"reply ";
+    }
+    
+
+}
