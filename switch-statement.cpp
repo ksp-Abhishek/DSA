82c8638e519b26f7037ae7b96d6410a7d1e3e275
ksp-Abhishek <abhishekkashyapa13@gmail.com>
2024-08-01 22:33:44 +0530

    switch-statement

---
+++ switch-statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='a';
+    cout<<endl;
+    switch(ch)
+    {
+        case 1:cout<<"first"<<endl;
+        break;
+        case 'a':cout<<"character one"<<endl;
+        break;
+
+
+    
+    default: cout<<"it is default case"<<endl;}
+
+}
