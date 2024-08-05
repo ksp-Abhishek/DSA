27e963fc46a497ecb79ac2fb9af905fbf1455ad6
ksp-Abhishek <abhishekkashyapa13@gmail.com>
2024-08-05 22:13:04 +0530

    array-intro

---
+++ arrays-intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void printarray(){
+    
+}int main(){
+    int number[15];
+    cout<<"value at 14 index"<<number[14]<<endl;
+
+    cout<<"everything is fine"<<endl;
+    return 0;
+}
