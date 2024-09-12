d63bce4a0c788ef4ac23641beaeca589dabd4e84
ksp-Abhishek <abhishekkashyapa13@gmail.com>
2024-09-12 13:41:17 +0530

    bubblesort

---
+++ practical2.cpp
@@ -0,0 +1,33 @@
+#include  <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {15, 82, 58, 25, 8, 33, 74, 47};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+
+    bubbleSort(arr, n);
+
+    cout<<"abhishekkash3yapa13@gmail.com"<<endl;
+    cout << "bubble sort solution  ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+}
